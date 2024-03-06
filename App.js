583c675a5362d4b14944e278973b2788d936fe6d
Mohammed Usama <usamabhakrani@gmail.com>
2024-03-06 16:52:56 +0500

    Status bar currentheight

--- App.js
+++ App.js
@@ -1,14 +1,32 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+} from "react-native";
 
 export default function App() {
-  return <View>
-    <StatusBar style="auto"/>
-  </View>;
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={{ padding: 16, backgroundColor: "dodgerblue" }}>
+        <Text>Search</Text>
+      </View>
+      <View style={{ flex: 1, padding: 16, backgroundColor: "gold" }}>
+        <Text>Blank Canvas</Text>
+      </View>
+      <ExpoStatusBar style="auto" />
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
   card: {},
   text: {},
 });
